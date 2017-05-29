f243b9ae29146e7773c5a3e5f25f206343b311ee
Pavel Viznitsa <viznitsap@gmail.com>
2017-05-29 12:52:59 +0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -5,31 +5,12 @@ $(document).ready(function() {
 
     for (var i = 0; i < images.length; i++) {
         $('.imgBlock').append('<div data-img="' + i + '">');
-        // $('.imgBlock div').addClass('class_name ' + i);
     };
 
-    // $('.imgBlock div:first').addClass('active');
 
     $('.imgBlock div').each(function(index, el) {
-        // $(this).addClass('img-' + index);
         currImg = $(this).attr('data-img');
         $(this).css('background-image', 'url(img/' + images[currImg] + ')');
-        // if ($(this).class = "active") {
-
-        // }
-
     });
-
-
-
-    // $('.data-img').each(function(index, el) {
-    //     console.log(index + ": " + $(this).text('some-text'));
-    // });
-
-
-
-
-
-
 
 });
